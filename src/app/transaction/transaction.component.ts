6495a6e9a0ae6a77dd0f5a856165df504c746672
Florent Nguyen <nguyen.florent@gmail.com>
2025-01-14 17:54:32 +0100

    test de la mort

--- src/app/transaction/transaction.component.ts
+++ src/app/transaction/transaction.component.ts
@@ -3,19 +3,52 @@ import { Observable } from "rxjs";
 import { HttpClient } from "@angular/common/http";
 import { HttpClientModule } from '@angular/common/http';
 
+export class Transaction {
+    id_trans: Number;
+    amount: Number;
+    date_trans: String;
+    flow: String;
+    id_account: String;
+    id_payee: String;
+    memo_trans: String
+}
+
 @Component({
     selector: 'transaction-home',
     template: `
         <div>Transactions</div>
-        {{ this.m_output }}
+        {{ this.m_transactions }}
         <div>Apres</div>
     `,
     imports: [HttpClientModule]
 })
 export class TransactionComponent implements OnInit {
-    
+    m_serviceUrl = 'http://91.134.68.226:5000/transactions';
+    m_transactions: Transaction[];
+    constructor(private http: HttpClient) { }
+
+    ngOnInit() {
+        this.getTransactions()
+    }
+
+    public getTransactions() {
+        return this.http.get<Transaction[]>(this.m_serviceUrl)
+            .subscribe(
+                (response) => {
+                    console.log('response received')
+                    console.log(response);
+                    this.m_transactions = response;
+                },
+                (error) => {
+                    console.error('Request failed with error')
+                    alert(error);
+                },
+                () => {
+                    console.log('Request completed')
+                })
+    }
+
     /*
-    
     m_transactions = [
         {
             amount: 810,
@@ -27,18 +60,5 @@ export class TransactionComponent implements OnInit {
             memo_trans: "menu"
         }
     ];
-    
-    
     */
-    m_req: Observable<Object[]>;
-    m_serviceUrl = 'http://91.134.68.226:5000/transactions';
-    posts : any;
-    constructor(private http: HttpClient) { }
-
-    m_output: string = '';
-    ngOnInit() {
-        this.http.get<Object[]>(this.m_serviceUrl).subscribe((response) => { this.posts = response; },
-            (error) => { console.log(error); });
-        this.m_output = 'on est passes dans ngOnInit'
-    }
 }
