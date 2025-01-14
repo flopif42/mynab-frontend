11948decd61f9347dc1ad56b9ef0cfbcc44c31fa
Florent Nguyen <nguyen.florent@gmail.com>
2025-01-14 17:32:23 +0100

    dev

--- src/app/transaction/transaction.component.ts
+++ src/app/transaction/transaction.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from "rxjs";
 import { HttpClient } from "@angular/common/http";
-// import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
     selector: 'transaction-home',
