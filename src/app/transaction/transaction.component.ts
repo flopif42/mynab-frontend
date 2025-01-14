ef6109db91aea0e0c0dcfe021ff9276ae0c8f3b2
Florent Nguyen <nguyen.florent@gmail.com>
2025-01-14 15:56:24 +0100

    dev

--- src/app/transaction/transaction.component.ts
+++ src/app/transaction/transaction.component.ts
@@ -5,5 +5,15 @@ import { Component } from '@angular/core';
     templateUrl: './transaction.component.html'
 })
 export class TransactionComponent {
-    m_transactions = [];
+    m_transactions = [
+        {
+            amount: 810,
+            date_trans: "14/01/2025",
+            flow: "outflow",
+            id_account: "tickets_resto",
+            id_payee: "quick",
+            id_trans: 1,
+            memo_trans: "menu"
+        }
+    ];
 }
