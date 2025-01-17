9561a78ef9614b75793528fd32d47db34a7e2bf7
Florent Nguyen <nguyen.florent@gmail.com>
2025-01-17 15:24:04 +0100

    selectors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    imports: [RouterLink, RouterOutlet, app-transaction],
+    imports: [RouterLink, RouterOutlet],
 })
 export class AppComponent {
     title = 'MYNAB';
