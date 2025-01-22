4f71caa9e8ed87184d10082afa68f34a65d4db7b
Florent Nguyen <nguyen.florent@gmail.com>
2025-01-22 11:52:19 +0100

    providers now ...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { HttpClientModule } from "@angular/common/http";
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    imports: [RouterLink, RouterOutlet, HttpClientModule],
+    imports: [RouterLink, RouterOutlet],
 })
 export class AppComponent {
     title = 'MYNAB';
