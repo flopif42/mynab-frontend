a9130a067ddbe6654de040d7fc66ee848a8dfb3f
Florent Nguyen <nguyen.florent@gmail.com>
2025-01-22 12:05:40 +0100

    md5

--- src/app/login/login.authservice.ts
+++ src/app/login/login.authservice.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
+import { Md5 } from 'ts-md5';
 
 /*
 import { shareReplay } from 'rxjs/operators'
@@ -10,13 +11,14 @@ import { shareReplay } from 'rxjs/operators'
 })
 export class AuthService {
     m_serviceUrl = 'http://91.134.68.226:5000';
+    m_endpoint = this.m_serviceUrl + "/user/login"
 
     constructor(private http: HttpClient) { }
 
-    login(email: string, password: string) {
-        const endpoint = this.m_serviceUrl + "/user/login"
+    login(email_address: string, password: string) {
+        var passphrase_md5 = Md5.hashStr(password);
 
-        return this.http.post<Object>(endpoint, { email, password })
+        return this.http.post<Object>(this.m_endpoint, { email_address, passphrase_md5 })
             // .pipe(shareReplay())
             ;
             // this is just the HTTP call, 
