3b442f2a05e6303746a39a06937ccbf413979518
Florent Nguyen <nguyen.florent@gmail.com>
2025-01-22 10:59:15 +0100

    x

--- src/app/login/login.authservice.ts
+++ src/app/login/login.authservice.ts
@@ -9,7 +9,7 @@ export class AuthService {
     constructor(private http: HttpClient) {
     }
     login(email: string, password: string) {
-        return this.http.post<Object>('/api/login', { email, password }).pipe(
+        return this.http.post<Object>('http://91.134.68.226:5000/user/login', { email, password }).pipe(
             shareReplay()
         );
             // this is just the HTTP call, 
