c62ebe36ba9a64f3fd3527656b1a001fe99da1f6
Florent Nguyen <nguyen.florent@gmail.com>
2025-01-22 10:32:29 +0100

    x

--- src/app/login/login.authservice.ts
+++ src/app/login/login.authservice.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
+import { shareReplay } from 'rxjs/operators'
 
 @Injectable({
     providedIn: 'root'
@@ -8,9 +9,10 @@ export class AuthService {
     constructor(private http: HttpClient) {
     }
     login(email: string, password: string) {
-        return this.http.post<Object>('/api/login', { email, password })
+        return this.http.post<Object>('/api/login', { email, password }).pipe(
+            shareReplay()
+        );
             // this is just the HTTP call, 
             // we still need to handle the reception of the token
-            .shareReplay();
     }
 }
