a01d35889f64f4fbe6c2c034729ed79a8f51119a
Florent Nguyen <nguyen.florent@gmail.com>
2025-01-22 09:52:06 +0100

    login jwt

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+    selector: 'login',
+    templateUrl: 'login.component.html'
+})
+export class LoginComponent {
+    form: FormGroup;
+
+    constructor(private fb: FormBuilder,
+        private authService: AuthService,
+        private router: Router) {
+
+        this.form = this.fb.group({
+            email: ['', Validators.required],
+            password: ['', Validators.required]
+        });
+    }
+
+    login() {
+        const val = this.form.value;
+
+        if (val.email && val.password) {
+            this.authService.login(val.email, val.password)
+                .subscribe(
+                    () => {
+                        console.log("User is logged in");
+                        this.router.navigateByUrl('/');
+                    }
+                );
+        }
+    }
+}
