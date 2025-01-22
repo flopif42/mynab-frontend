a8871fd763a9a07349497e66f7617af3241c21be
Florent Nguyen <nguyen.florent@gmail.com>
2025-01-22 11:41:34 +0100

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { HttpClientModule } from "@angular/common/http";
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    imports: [RouterLink, RouterOutlet],
+    imports: [RouterLink, RouterOutlet, HttpClientModule],
 })
 export class AppComponent {
     title = 'MYNAB';
--- src/app/login/login.authservice.ts
+++ src/app/login/login.authservice.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
-import { HttpClientModule } from "@angular/common/http";
+
 /*
 import { shareReplay } from 'rxjs/operators'
 */
