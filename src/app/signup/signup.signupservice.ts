58c628efa7969e15a1beb1a8cb38a04bedae024c
Florent Nguyen <nguyen.florent@gmail.com>
2025-01-23 17:22:53 +0100

    x

--- src/app/signup/signup.signupservice.ts
+++ src/app/signup/signup.signupservice.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { Md5 } from 'ts-md5';
-import { environment } from '../environments/environment'
+import { environment } from '../../environments/environment'
 
 @Injectable({
     providedIn: 'root'
