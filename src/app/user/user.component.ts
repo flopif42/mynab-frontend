43a4364a71da694c8150f377f160fe3447afd1e0
Florent Nguyen <nguyen.florent@gmail.com>
2025-01-27 15:15:19 +0100

    user profile ?

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,9 +1,32 @@
 import { Component } from '@angular/core';
+import { environment } from '../../environments/environment'
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'user-home',
-  template: `
-    <div>User Home Page</div>
-  `
+    selector: 'user-home',
+    templateUrl: 'user.component.html',
 })
-export class UserComponent { }
+export class UserComponent {
+    m_endpoint = environment.apiUrl + "/user/profile"
+    m_data = ''
+
+    constructor(private http: HttpClient) { }
+
+    ngOnInit() {
+        this.getUserProfile()
+    }
+
+    public getUserProfile() {
+        return this.http.get<Object>(this.m_endpoint)
+            .subscribe(
+                (response) => {
+                    console.log('response received')
+                    this.m_data = response;
+                },
+                (error) => {
+                    console.error('Request failed with error')
+                    alert(error);
+                },
+                () => { console.log('Request completed') })
+    }
+}
