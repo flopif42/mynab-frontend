349d2ba1a1cd260776b3399489062b7e245de2f4
Florent Nguyen <nguyen.florent@gmail.com>
2025-01-27 10:41:36 +0100

    response

--- src/app/login/login.authservice.ts
+++ src/app/login/login.authservice.ts
@@ -1,13 +1,16 @@
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpRequest, HttpHandlerFn, HttpEvent } from "@angular/common/http";
+import { HttpClient, HttpRequest, HttpHandlerFn, HttpEvent, HttpEventType } from "@angular/common/http";
 import { Md5 } from 'ts-md5';
 import { shareReplay } from 'rxjs/operators';
 import { environment } from '../../environments/environment';
 
 export function loggingInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> {
-    console.log(req.url);
-    return next(req);
+    return next(req).pipe(tap(event => {
+        if (event.type === HttpEventType.Response) {
+            console.log(req.url, 'returned a response with status', event.status);
+        }
+    }));
 }
 
 @Injectable({
