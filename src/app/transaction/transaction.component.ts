ed2fba09dbf2c7404b502d8f47f12fb77cebc3ab
Florent Nguyen <nguyen.florent@gmail.com>
2025-01-30 17:29:45 +0100

    This is a stable commit.

--- src/app/transaction/transaction.component.ts
+++ src/app/transaction/transaction.component.ts
@@ -22,7 +22,7 @@ export class Transaction {
     imports: [HttpClientModule, ReactiveFormsModule]
 })
 export class TransactionComponent implements OnInit {
-    m_endpoint = environment.apiUrl + "/user/profile"
+    m_endpoint = environment.apiUrl + "/transaction"
     m_transactions: Transaction[];
 
     m_newTransactionForm = new FormGroup({
@@ -55,9 +55,8 @@ export class TransactionComponent implements OnInit {
     }
 
     public createTransaction(txn) {
-        const endpoint = this.m_serviceUrl + "/transactions/new"
         const headers = { 'Content-Type': 'application/json' };
-        var result = this.http.post<any>(endpoint, txn, { headers }).subscribe({
+        var result = this.http.post<any>(this.m_endpoint + "/new", txn, { headers }).subscribe({
             next: data => { console.log(data.lastRowId); this.getTransactions() },
             error: error => { console.error('There was an error!', error); }
         })
