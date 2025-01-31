e10e9542c9c28fc00126ec25d0962ca210f8e158
Florent Nguyen <nguyen.florent@gmail.com>
2025-01-31 15:13:37 +0100

    x

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,7 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
 //import { AuthInterceptor } from './auth/auth.authservice'
-import { authInterceptor } from './auth/auth-interceptor.service'
+//import { authInterceptor } from './auth/auth-interceptor.service'
 import { LogInterceptor } from './auth/auth.authservice'
 
 export const appConfig: ApplicationConfig = {
