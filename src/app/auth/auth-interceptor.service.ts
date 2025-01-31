1b82a7f8cd59e997ae797d4b6798909635edee33
Florent Nguyen <nguyen.florent@gmail.com>
2025-01-31 16:31:03 +0100

    x

--- src/app/auth/auth-interceptor.service.ts
+++ src/app/auth/auth-interceptor.service.ts
@@ -6,19 +6,24 @@ import { catchError, tap } from 'rxjs/operators';
 export function authInterceptor(req: HttpRequest<any>, next: HttpHandlerFn): Observable<HttpEvent<any>> {
     console.log("> in AuthInterceptor:intercept()", req.url)
     req = req.clone({ withCredentials: true, });
-    return next(req).pipe(tap(event => {
-        if (event.type === HttpEventType.Response) {
-            console.log("event type = response")
-            if (event.status == 401) {
-                console.log("event status = 401")
+    return next(req).pipe(tap(
+        error => {
+            console.log("error = ", error)
+        },
+        event => {
+            if (event.type === HttpEventType.Response) {
+                console.log("event type = response")
+                if (event.status == 401) {
+                    console.log("event status = 401")
+                }
+                else {
+                    console.log("event status = ", event.status)
+                }
             }
             else {
-                console.log("event status = ", event.status)
+                console.log("event type = ", event.type)
             }
+            return next(req);
         }
-        else {
-            console.log("event type = ", event.type)
-        }
-        return next(req);
-    }));
+    ));
 }
