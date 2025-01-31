546f8e284f8d2d3cdb71a90793db2e44a0764c27
Florent Nguyen <nguyen.florent@gmail.com>
2025-01-31 16:20:21 +0100

    x

--- src/app/auth/auth-interceptor.service.ts
+++ src/app/auth/auth-interceptor.service.ts
@@ -1,31 +1,15 @@
-import { Injectable, inject } from '@angular/core';
-import { HttpRequest, HttpHandlerFn, HttpEvent, HttpErrorResponse } from '@angular/common/http';
+import { HttpRequest, HttpHandlerFn, HttpEvent, HttpErrorResponse, HttpEventType } from '@angular/common/http';
 import { Observable, throwError, BehaviorSubject } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { catchError, tap } from 'rxjs/operators';
 // import { AuthService } from './auth.authservice';
 
 export function authInterceptor(req: HttpRequest<any>, next: HttpHandlerFn): Observable<HttpEvent<any>> {
     console.log("> in AuthInterceptor:intercept()", req.url)
     req = req.clone({ withCredentials: true, });
-    return next(req).pipe(
-        catchError((error) => {
-            if (error instanceof HttpErrorResponse && error.status === 401) {
-                console.log("< in AuthInterceptor: catchError(error)")
-
-                const response = inject(TokenRefreshService).someMethod()
-                console.log(response)
-
-                return next(req);
-            }
-            console.log("< in AuthInterceptor: throwError(error)")
-            return throwError(() => error);
-        })
-    );
-}
-
-@Injectable()
-export class TokenRefreshService {
-    someMethod() {
-        console.log("In someMethod()")
-    }
+    return next(req).pipe(tap(event => {
+        if (event.type === HttpEventType.Response && event.status == 401) {
+            console.log("< in AuthInterceptor: catchError(error)")
+            return next(req);
+        }
+    }));
 }
