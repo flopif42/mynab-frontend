fd7ac238d298c5b9f2b50754fc469e1133ca680a
Florent Nguyen <nguyen.florent@gmail.com>
2025-01-31 14:48:06 +0100

    x

--- src/app/auth/auth-interceptor.service.ts
+++ src/app/auth/auth-interceptor.service.ts
@@ -1,8 +1,9 @@
+import { inject } from '@angular/core';
 import { HttpRequest, HttpHandlerFn, HttpEvent, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError, BehaviorSubject } from 'rxjs';
 import { catchError, switchMap, filter, take } from 'rxjs/operators';
+import { AuthService } from './auth.authservice';
 
-// export function LogInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> {
 export function AuthInterceptor(req: HttpRequest<any>, next: HttpHandlerFn): Observable<HttpEvent<any>> {
     console.log("> in AuthInterceptor:intercept()")
     req = req.clone({ withCredentials: true, });
@@ -10,6 +11,9 @@ export function AuthInterceptor(req: HttpRequest<any>, next: HttpHandlerFn): Obs
         catchError((error) => {
             if (error instanceof HttpErrorResponse && error.status === 401) {
                 console.log("< in AuthInterceptor: catchError(error)")
+                const authService = inject(AuthService);
+                const response = authService.refresh()
+                console.log(response)
                 return next(req);
             }
             console.log("< in AuthInterceptor: throwError(error)")
