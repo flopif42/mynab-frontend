aca5779ffffe956c71bfbdcd7d4f9f62b7007ee9
Florent Nguyen <nguyen.florent@gmail.com>
2025-01-31 14:12:35 +0100

    x

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,8 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
-import { AuthInterceptor } from './auth/auth.authservice'
+//import { AuthInterceptor } from './auth/auth.authservice'
+import { AuthInterceptor } from './auth/auth-interceptor.service'
 import { LogInterceptor } from './auth/auth.authservice'
 
 export const appConfig: ApplicationConfig = {
---
+++ src/app/auth/auth-interceptor.service.ts
@@ -0,0 +1,7 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse } from '@angular/common/http';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+}
