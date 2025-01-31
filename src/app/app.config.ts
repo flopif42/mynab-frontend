b52cabbfa1e2a66287d8a4bb59789a4b26c6db81
Florent Nguyen <nguyen.florent@gmail.com>
2025-01-31 16:38:38 +0100

    with credentials

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,13 +2,13 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
-import { logInterceptor } from './log-interceptor.service'
+import { logAuthInterceptor } from './log-auth-interceptor.service'
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideRouter(routes),
         provideHttpClient(
-            withInterceptors([logInterceptor])
+            withInterceptors([logAuthInterceptor])
         ),
     ]
 };
---
+++ src/app/log-auth-interceptor.service.ts
@@ -0,0 +1,8 @@
+import { Observable } from 'rxjs';
+import { HttpRequest, HttpHandlerFn, HttpEvent } from "@angular/common/http";
+
+export function logAuthInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> {
+    console.log(req.url);
+    req = req.clone({ withCredentials: true });
+    return next(req);
+}
