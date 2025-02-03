f9163090b37bd2af3d5ea6e13eafb6c9d6e3896a
Florent Nguyen <nguyen.florent@gmail.com>
2025-02-03 15:11:30 +0100

    x

--- src/app/log-auth-interceptor.service.ts
+++ src/app/log-auth-interceptor.service.ts
@@ -2,17 +2,29 @@ import { Observable, tap, catchError, throwError } from 'rxjs';
 import { HttpRequest, HttpHandlerFn, HttpEvent, HttpEventType, HttpErrorResponse } from "@angular/common/http";
 
 export function logAuthInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> {
-    console.log("request log> " + req.urlWithParams + " " + req.method);
+    console.log("request log> " + req.url + " " + req.method);
     req = req.clone({ withCredentials: true });
     return next(req).pipe(
         tap(event => {
             if (event.type === HttpEventType.Response) {
-                console.log("response log> " + req.urlWithParams + " " + req.method);
+                console.log("response log> " + req.url + " " + req.method);
             }
         }),
-        catchError((error: HttpErrorResponse) => {
-            console.error("error response log> " + req.urlWithParams + " " + req.method);
-            return throwError(() => error);
+        catchError((err: HttpErrorResponse) => {
+            let errorMessage = "error response log> " + req.url + " " + req.method
+
+            if (err.status === 401) {
+                if (req.url === "https://budgetizator.ovh:543/user/refresh") {
+                    errorMessage = "Attempt to refresh the access token failed."
+                } else {
+                    errorMessage = "error 401 log> " + req.url + " " + req.method
+                }
+            } else {
+                errorMessage = "error not 401 log> " + req.url + " " + req.method
+            }
+
+            console.error(errorMessage)
+            return throwError(() => err);
         })
     );
 }
