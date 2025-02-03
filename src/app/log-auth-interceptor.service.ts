5c74c6d99fd0845aac0bad355e2b8616acce5991
Florent Nguyen <nguyen.florent@gmail.com>
2025-02-03 14:38:37 +0100

    z

--- src/app/log-auth-interceptor.service.ts
+++ src/app/log-auth-interceptor.service.ts
@@ -1,16 +1,17 @@
-import { Observable, tap } from 'rxjs';
+import { Observable, tap, catchError } from 'rxjs';
 import { HttpRequest, HttpHandlerFn, HttpEvent, HttpEventType, HttpErrorResponse } from "@angular/common/http";
 
 export function logAuthInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> {
     console.log("request log> " + req.urlWithParams + " " + req.method);
     req = req.clone({ withCredentials: true });
-    return next(req).pipe(tap(event => {
-        if (event.type === HttpEventType.Response) {
-            console.log("response log> " + req.urlWithParams + " " + req.method);
-        }
-        if (event.type === HttpErrorResponse) {
-            console.error("response log (error)> " + req.urlWithParams + " " + req.method);
-        }
-    }
-    ));
+    return next(req).pipe(
+        tap(event => {
+            if (event.type === HttpEventType.Response) {
+                console.log("response log> " + req.urlWithParams + " " + req.method);
+            }
+        }),
+        catchError((e: HttpErrorResponse) => {
+            console.error("error response log> " + req.urlWithParams + " " + req.method);
+        })
+    );
 }
