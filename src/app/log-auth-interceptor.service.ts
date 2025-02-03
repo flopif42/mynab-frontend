4614dfaec0f88a1a2e4168fc1e1e3e9f90543863
Florent Nguyen <nguyen.florent@gmail.com>
2025-02-03 16:44:58 +0100

    y

--- src/app/log-auth-interceptor.service.ts
+++ src/app/log-auth-interceptor.service.ts
@@ -4,6 +4,7 @@ import { HttpInterceptor, HttpClient, HttpRequest, HttpHandlerFn, HttpHandler, H
 import { environment } from '../environments/environment';
 import { Router } from '@angular/router';
 
+/*
 export function logAuthInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> {
     console.log("request log> " + req.url + " " + req.method);
     req = req.clone({ withCredentials: true });
@@ -32,6 +33,7 @@ export function logAuthInterceptor(req: HttpRequest<unknown>, next: HttpHandlerF
         })
     );
 }
+*/
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
@@ -40,6 +42,7 @@ export class AuthInterceptor implements HttpInterceptor {
     m_endpoint = environment.apiUrl + "/user"
 
     attemptRefresh(req: HttpRequest<unknown>, next: HttpHandler) {
+        console.log("Attempting to refresh the token ...")
         this.http.get(this.m_endpoint + "/refresh", { observe: 'response' }).subscribe(
             res => {
                 console.log("Access token successfully refreshed. Retrying the same request ...")
@@ -50,7 +53,6 @@ export class AuthInterceptor implements HttpInterceptor {
                 console.error("Attempt to refresh the access token failed.");
                 this.router.navigate(['/login']);
                 return throwError(() => error);
-
             })
     }
 
@@ -64,22 +66,21 @@ export class AuthInterceptor implements HttpInterceptor {
                 }
             }),
             catchError((err: HttpErrorResponse) => {
-                let errorMessage = "error response log> " + req.url + " " + req.method
-
+                let errorMessage
                 if (err.status === 401) {
                     if (req.url === "https://budgetizator.ovh:543/user/refresh") {
                         errorMessage = "Attempt to refresh the access token failed."
                         this.router.navigate(['/login']);
                         return throwError(() => err);
                     } else {
-                        errorMessage = "This is error 401. Client should try and refresh access token."
+                        errorMessage = "This is error 401 on a page that's not the refresh page."
                         return this.attemptRefresh(req, next)
                     }
                 } else {
                     errorMessage = "error not 401 log> " + req.url + " " + req.method
                 }
-
                 console.error(errorMessage)
+                return throwError(() => err);
             })
         );
     }
