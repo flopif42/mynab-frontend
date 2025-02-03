7f69f70047a6dea0a1cf2badfac29d46ec704750
Florent Nguyen <nguyen.florent@gmail.com>
2025-02-03 15:38:02 +0100

    y

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,17 @@
+import { withInterceptorsFromDi, HTTP_INTERCEPTORS } from @angular/common/http
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
-import { logAuthInterceptor } from './log-auth-interceptor.service'
+import { logAuthInterceptor, AuthInterceptor } from './log-auth-interceptor.service'
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideRouter(routes),
         provideHttpClient(
-            withInterceptors([logAuthInterceptor])
+          //  withInterceptors([logAuthInterceptor])
+            withInterceptorsFromDi(),
         ),
+        { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
     ]
 };
--- src/app/log-auth-interceptor.service.ts
+++ src/app/log-auth-interceptor.service.ts
@@ -1,3 +1,4 @@
+import { Injectable } from "@angular/core";
 import { Observable, tap, catchError, throwError } from 'rxjs';
 import { HttpInterceptor, HttpClient, HttpRequest, HttpHandlerFn, HttpHandler, HttpEvent, HttpEventType, HttpErrorResponse } from "@angular/common/http";
 
@@ -30,8 +31,34 @@ export function logAuthInterceptor(req: HttpRequest<unknown>, next: HttpHandlerF
     );
 }
 
+@Injectable
 export class AuthInterceptor implements HttpInterceptor {
     intercept(req: HttpRequest<unknown>, next: HttpHandler) {
-        return next.handle(req)
+        console.log("request log> " + req.url + " " + req.method);
+        req = req.clone({ withCredentials: true });
+        return next.handle(req).pipe(
+            tap(event => {
+                if (event.type === HttpEventType.Response) {
+                    console.log("response log> " + req.url + " " + req.method);
+                }
+            }),
+            catchError((err: HttpErrorResponse) => {
+                let errorMessage = "error response log> " + req.url + " " + req.method
+
+                if (err.status === 401) {
+                    if (req.url === "https://budgetizator.ovh:543/user/refresh") {
+                        errorMessage = "Attempt to refresh the access token failed."
+                        // TODO: redirect user to login page
+                    } else {
+                        errorMessage = "This is error 401. Client should try and refresh access token."
+                    }
+                } else {
+                    errorMessage = "error not 401 log> " + req.url + " " + req.method
+                }
+
+                console.error(errorMessage)
+                return throwError(() => err);
+            })
+        );
     }
 }
