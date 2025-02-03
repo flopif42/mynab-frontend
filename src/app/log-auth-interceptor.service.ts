90205008b4c62b05261166f8ef25b60f2202d810
Florent Nguyen <nguyen.florent@gmail.com>
2025-02-03 14:44:09 +0100

    x

--- src/app/log-auth-interceptor.service.ts
+++ src/app/log-auth-interceptor.service.ts
@@ -5,11 +5,13 @@ export function logAuthInterceptor(req: HttpRequest<unknown>, next: HttpHandlerF
     console.log("request log> " + req.urlWithParams + " " + req.method);
     req = req.clone({ withCredentials: true });
     return next(req).pipe(
+        /*
         tap(event => {
             if (event.type === HttpEventType.Response) {
                 console.log("response log> " + req.urlWithParams + " " + req.method);
             }
         }),
+        */
         catchError((e: HttpErrorResponse) => {
             console.error("error response log> " + req.urlWithParams + " " + req.method);
         })
