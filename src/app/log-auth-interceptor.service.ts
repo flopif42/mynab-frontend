36b75941a18032d78db7a7faba9c575b147f5236
Florent Nguyen <nguyen.florent@gmail.com>
2025-02-03 14:03:15 +0100

    x

--- src/app/log-auth-interceptor.service.ts
+++ src/app/log-auth-interceptor.service.ts
@@ -2,7 +2,7 @@ import { Observable } from 'rxjs';
 import { HttpRequest, HttpHandlerFn, HttpEvent } from "@angular/common/http";
 
 export function logAuthInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> {
-    console.log(req.url);
+    console.log(req.urlWithParams);
     req = req.clone({ withCredentials: true });
     return next(req);
 }
