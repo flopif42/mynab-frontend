8915f7cf543da05d7bc0eb6caf244a502dce1631
Florent Nguyen <nguyen.florent@gmail.com>
2025-02-04 10:57:18 +0100

    z

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,51 +1,21 @@
 import { Injectable } from '@angular/core';
-import { HttpBackend, HttpClient, HttpRequest, HttpResponse, HttpHandler, HttpEvent } from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 import { Md5 } from 'ts-md5';
 import { shareReplay } from 'rxjs/operators';
 import { environment } from '../../environments/environment';
-import { Observable, tap, catchError, throwError } from 'rxjs';
-import { Router } from '@angular/router';
 
 @Injectable({
     providedIn: 'root'
 })
 export class AuthService {
     m_endpoint = environment.apiUrl + "/user"
-    private http: HttpClient;
-    constructor(private httpBackend: HttpBackend, private router: Router) {
-        this.http = new HttpClient(httpBackend)
-    }
 
-    // Login with credentials to request an Access Token and a Refresh Token
+    constructor(private http: HttpClient) {}
+
+    // Login with credentials to request an Access Token
     login(email_address: string, password: string) {
         var passphrase_md5 = Md5.hashStr(password);
-        return this.http.post<Object>(this.m_endpoint + "/login", { email_address, passphrase_md5 }, { observe: 'response', withCredentials: true })
+        return this.http.post<Object>(this.m_endpoint + "/login", { email_address, passphrase_md5 })
             .pipe(shareReplay());
     }
-
-    // Attempt to refresh the access token
-    attemptRefresh(req: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-        console.log("Attempting to refresh the token ...")
-        req = req.clone({ withCredentials: true })
-        var obs = this.http.get(this.m_endpoint + "/refresh", { observe: 'response', withCredentials: true })
-        obs.subscribe(
-            res => {
-                console.log("Access token successfully refreshed. Retrying the same request ...")
-                console.log(req.url)
-                return next.handle(req)
-            },
-            error => {
-                console.error("in attemptRefresh() : Attempt to refresh the access token failed.");
-                this.router.navigate(['/login']);
-            })
-        return obs
-    }
-
-    // Call the refresh endpoint to request a new Access Token, provided the Refresh token is not expired.
-    /*
-    refresh() {
-        console.log('in AuthService.refresh() called by httpbackend')
-        return this.http.get(this.m_endpoint + "/refresh", { observe: 'response', withCredentials: true })
-    }
-    */
 }
--- src/app/log-auth-interceptor.service.ts
+++ src/app/log-auth-interceptor.service.ts
@@ -1,12 +1,11 @@
+import { Router } from '@angular/router';
 import { Injectable } from "@angular/core";
-import { Observable, tap, catchError, throwError } from 'rxjs';
-import { HttpBackend, HttpInterceptor, HttpClient, HttpRequest, HttpHandlerFn, HttpHandler, HttpEvent, HttpEventType, HttpErrorResponse } from "@angular/common/http";
-import { environment } from '../environments/environment';
-import { AuthService } from './auth/auth.service';
+import { tap, catchError, throwError } from 'rxjs';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEventType, HttpErrorResponse } from "@angular/common/http";
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-    constructor(private authService: AuthService) {}
+    constructor(private router: Router) {}
 
     intercept(req: HttpRequest<unknown>, next: HttpHandler) {
         console.log("request log> " + req.url + " " + req.method);
@@ -19,24 +18,12 @@ export class AuthInterceptor implements HttpInterceptor {
             }),
             catchError((err: HttpErrorResponse) => {
                 console.error("in intercept() catchError()")
-                let errorMessage
                 if (err.status === 401) {
-                    if (req.url === "https://budgetizator.ovh:543/user/refresh") {
-                        errorMessage = "in intercept() : Attempt to refresh the access token failed."
-                        console.error(errorMessage)
-//                        this.router.navigate(['/login']);
-  //                      return throwError(() => err);
-                    } else {
-                        errorMessage = "This is error 401 on a page that's not the refresh page. Try to refresh the access token."
-                        console.error(errorMessage)
-//                        return next.handle(req)
-//                        return throwError(() => err);
-                        return this.authService.attemptRefresh(req, next)
-                    }
+                    console.error("This is error 401, the token has expired. Redirect user to login page.")
+                    this.router.navigate(['/login'])
+
                 } else {
-                    errorMessage = "error not 401 log> " + req.url + " " + req.method
-                    console.error(errorMessage)
-                    
+                    console.error("error not 401 log> " + req.url + " " + req.method)
                 }
                 return throwError(() => err);
             })
