9b6f1f29dcdf994f3a2584746228ac80096479a0
Florent Nguyen <nguyen.florent@gmail.com>
2025-02-04 14:15:27 +0100

    y

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -2,7 +2,7 @@ import { Router } from '@angular/router'
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
-import { confirmPasswordValidator } from '../confirm-password.validator';
+import { Account } from './account.model'
 import { AccountService } from './account.service';
 
 @Component({
@@ -10,10 +10,6 @@ import { AccountService } from './account.service';
     templateUrl: 'account.component.html',
     imports: [ReactiveFormsModule]
 })
-
-export class Account {
-    _name_account: string
-}
 
 export class AccountComponent {
     createAccountForm = new FormGroup({
--- src/app/account/account.service.ts
+++ src/app/account/account.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { environment } from '../../environments/environment'
+import { Account } from './account.model'
 
 @Injectable({
     providedIn: 'root'
