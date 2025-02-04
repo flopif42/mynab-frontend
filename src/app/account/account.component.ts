c98c4b83a92a22a40ee06b0933f6a75edd549c48
Florent Nguyen <nguyen.florent@gmail.com>
2025-02-04 16:36:23 +0100

    y

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -64,7 +64,8 @@ export class AccountComponent {
 //                        this.router.navigate(['/']);
                     }
                 );
-                */
+                
         }
+        */
     }
 }
