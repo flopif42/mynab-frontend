bb80ed9cd16dabcc921c00e54a2800b9887ae826
Florent Nguyen <nguyen.florent@gmail.com>
2025-02-04 17:56:16 +0100

    z

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-dropdown',
