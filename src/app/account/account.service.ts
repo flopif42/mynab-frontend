a80fa429153219ddbe17c21bccf6771b8a8f553d
Florent Nguyen <nguyen.florent@gmail.com>
2025-02-05 10:18:36 +0100

    y

--- src/app/account/account.service.ts
+++ src/app/account/account.service.ts
@@ -12,6 +12,7 @@ export class AccountService {
     constructor(private http: HttpClient) { }
 
     create(account_name: string, account_type: number) {
+        console.log("In AccountService:create()")
         return this.http.post<Object>(this.m_endpoint + '/create', { account_name, account_type });
     }
 
