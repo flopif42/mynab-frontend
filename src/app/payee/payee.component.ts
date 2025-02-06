77580b97c665e94b7636384b8673085dc7825e04
Florent Nguyen <nguyen.florent@gmail.com>
2025-02-06 15:02:26 +0100

    x

--- src/app/payee/payee.component.ts
+++ src/app/payee/payee.component.ts
@@ -1,9 +1,54 @@
-import { Component } from '@angular/core';
+import { KeyValuePipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Validators, FormGroup, FormControl } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { PayeeService } from './payee.service'
+import { Payee } from './payee.model'
 
 @Component({
-  selector: 'payee-home',
-  template: `
-    <div>Manage Payees</div>
-  `
+    selector: 'app-payee',
+    templateUrl: './payee.component.html',
+    styleUrl: './payee.component.css',
+    imports: [ReactiveFormsModule, KeyValuePipe]
 })
-export class PayeeComponent { }
+export class PayeeComponent implements OnInit {
+    _newPayeeForm = new FormGroup({
+        payee_name: new FormControl('', [Validators.required])
+    });
+
+    _payees = [];
+
+    constructor(private payeeService: PayeeService) { }
+
+    ngOnInit() {
+        this.listPayees()
+    }
+
+    listPayees() {
+        this.payeeService.getList().subscribe(
+            response => {
+                const payeesFromApi: Payee[] = response
+                payeesFromApi.forEach((payee: Payee) => {
+                    this._payees.push(payee)
+                })
+            },
+            error => {
+                console.error("Error fetching accounts")
+            }
+        )
+    }
+
+    onSubmit() {
+        if (this._newPayeeForm.value && this._newPayeeForm.value.payee_name) {
+            this.payeeService.create(this._newPayeeForm.value.account_name)
+                .subscribe(
+                    res => {
+                        console.log("Payee created.")
+                    },
+                    error => {
+                        console.error("Error creating payee")
+                    }
+                )
+        }
+    }
+}
