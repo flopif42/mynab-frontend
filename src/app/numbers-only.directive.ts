dabe1323b0639a8ce23f14843a1f048b249a3a86
Florent Nguyen <nguyen.florent@gmail.com>
2025-02-13 14:28:46 +0100

    x

--- src/app/numbers-only.directive.ts
+++ src/app/numbers-only.directive.ts
@@ -3,12 +3,13 @@ import { Directive, HostListener } from '@angular/core';
 @Directive({
     selector: '[numbersOnly]'
 })
-export class numbersOnlyOnlyDirective {
+export class numbersOnlyDirective {
     // Allow numbers only
     private regex: RegExp = /^[0-9]*$/;
 
     @HostListener('keypress', ['$event'])
     onKeyPress(event: KeyboardEvent) {
+        console.log("In onKeyPress()")
         const key = String.fromCharCode(event.which);
         if (!this.regex.test(key)) {
             event.preventDefault();
