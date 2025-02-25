aa74cd9b1a69c8876625219d9cb78982b28fe78c
Florent Nguyen <nguyen.florent@gmail.com>
2025-02-25 16:43:39 +0100

    y

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -62,7 +62,7 @@ export class CategoryComponent implements OnInit {
                 .subscribe(
                     res => {
                         console.log("Category created.")
-                        this._newCategoryForm['category_name'] = ''
+                        this._newCategoryForm.value.category_name.setValue('')
                         this.listCategories()
                     },
                     error => {
