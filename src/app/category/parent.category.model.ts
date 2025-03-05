506047a8618cdfde31f8524c5a5ab01a160ac42e
Florent Nguyen <nguyen.florent@gmail.com>
2025-03-05 09:45:35 +0100

    z

--- src/app/category/parent.category.model.ts
+++ src/app/category/parent.category.model.ts
@@ -3,12 +3,14 @@ import { Category } from './category.model'
 export class ParentCategory {
     id: number;
     name: string;
+    can_be_deleted: number;
     position: number;
     child_categories;
 
-    constructor(parentCatId, parentCatName, parentCatPosition) {
+    constructor(parentCatId, parentCatName, canBeDeleted, parentCatPosition) {
         this.id = parentCatId;
         this.name = parentCatName;
+        this.can_be_deleted = canBeDeleted;
         this.position = parentCatPosition;
         this.child_categories = [];
     }
