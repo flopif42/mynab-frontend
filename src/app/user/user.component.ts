ca6b181474d3df4ea1a8936486ef6e65a26a9f68
Florent Nguyen <nguyen.florent@gmail.com>
2025-03-31 15:39:41 +0200

    x

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -8,7 +8,9 @@ import { HttpClient } from "@angular/common/http";
 })
 export class UserComponent {
     m_endpoint = environment.apiUrl + "/user/profile"
-    m_data = {}
+    _first_name: string
+    _last_name: string
+    _email_address: string
 
     constructor(private http: HttpClient) { }
 
@@ -19,7 +21,9 @@ export class UserComponent {
     public getUserProfile() {
         return this.http.get<Object>(this.m_endpoint).subscribe(
             response => {
-                this.m_data = response;
+                this._first_name = response['first_name'];
+                this._last_name = response['last_name'];
+                this._email_address = response['email_address'];
             },
             error => {
                 console.error('in getUserProfile() error')
