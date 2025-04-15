08a4ca6ee374095c706ef8a0bb0962b2b37d6302
Florent Nguyen <nguyen.florent@gmail.com>
2025-04-15 15:10:08 +0200

    x

--- src/app/transaction/transaction.model.ts
+++ src/app/transaction/transaction.model.ts
@@ -1,10 +1,13 @@
 export class Transaction {
     id: number;
     account: String;
+    account_type: number;
     amount: number;
     date: string;
     flow: String;
     memo: String;
     payee: String;
     category: String;
+    is_transfer: number;
+    linked_account_type: number;
 }
