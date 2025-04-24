a3cc2c3512241f9e87fa633e140dac8ff7bee7c2
Florent Nguyen <nguyen.florent@gmail.com>
2025-04-24 10:08:50 +0200

    y

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -3,22 +3,15 @@ import { HttpClient } from "@angular/common/http";
 import { environment } from '../../environments/environment'
 
 @Component({
-  selector: 'app-about',
-    template: `
-    <div>Client version (Jenkins build number) : {{ this._clientVersion }}<div>
-    <div>API server status : {{ this._apiServerStatus }}</div>
-    <div>API version : {{ this._apiVersion }}</div>
-    <div><a href="https://budgetizator.ovh:543/apidocs/">API Documentation</a></div>
-    <div>Database server status : {{ this._dbServerStatus }}</div>
-    <div>Database version : {{ this._dbVersion }}</div>
-    
-  `
+    selector: 'app-about',
+    templateUrl: 'about.component.html'
 })
 export class AboutComponent {
     m_endpoint = environment.apiUrl;
     _clientVersion: string;
     _apiServerStatus: string;
     _apiVersion: string;
+    _apiDocUrl: string;
     _dbServerStatus: string;
     _dbVersion: string;
 
@@ -27,10 +20,11 @@ export class AboutComponent {
     ngOnInit() {
         this.getServerInfo();
         this._clientVersion = environment.jenkinsBuildNumber
+        this._apiDocUrl = environment.apiUrl + '/apidocs/'
     }
 
     public getServerInfo() {
-        return this.http.get<Object>(this.m_endpoint + "/about").subscribe(
+        return this.http.get<Object>(this.m_endpoint + '/about').subscribe(
             (response) => {
                 this._apiServerStatus = 'Up';
                 this._apiVersion = response['API version'];
