cbae9de14fd6fb80e27072b6f28f0b0125e63374
Florent Nguyen <nguyen.florent@gmail.com>
2025-04-24 12:05:26 +0200

    x

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
-    imports: [RouterLink, RouterOutlet, MatSidenavModule, MatListModule, MatIconModule]
+    imports: [RouterLink, RouterOutlet, MatSidenavModule, MatListModule, MatIconModule, MatButtonModule]
 })
 
 @Injectable({
