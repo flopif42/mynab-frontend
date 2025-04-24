3a66debdd91a7578ce62f2b4e6d988d00029f68a
Florent Nguyen <nguyen.florent@gmail.com>
2025-04-24 11:27:38 +0200

    y

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component, Injectable } from '@angular/core';
 import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { AuthService } from './auth/auth.service';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    imports: [RouterLink, RouterOutlet],
+    imports: [RouterLink, RouterOutlet, MatSidenavModule],
 })
 
 @Injectable({
