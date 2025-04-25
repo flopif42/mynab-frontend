6514c1652d6288746e175d02fc70b6ffe92519de
Florent Nguyen <nguyen.florent@gmail.com>
2025-04-25 16:45:50 +0200

    y

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from './user.service';
+import { UserProfile } from './user.model'
 
 @Component({
     selector: 'user-home',
     templateUrl: 'user.component.html',
 })
 export class UserComponent implements OnInit {
-    _user: UserService;
+    _user: UserProfile;
     constructor(private userService: UserService) { }
 
     ngOnInit() {
