5f12bc1394d90e5e959e00004c895068ddc8ebca
Florent Nguyen <nguyen.florent@gmail.com>
2025-04-25 17:15:10 +0200

    y

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,19 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from './user.service';
-import { UserProfile } from './user.model'
+import { UserProfile } from './user.model';
 
 @Component({
     selector: 'user-home',
     templateUrl: 'user.component.html',
 })
 export class UserComponent implements OnInit {
-    _user: UserProfile;
+    // _user: UserProfile;
+
+    _user = {
+        "email_address": "mail bla bla",
+        "first_name": "flo",
+        "last_name": "ngu",
+        "ui_collapse_cash": 0,
+        "ui_collapse_closed": 1,
+        "ui_collapse_tracking": 1
+    };
+
     constructor(private userService: UserService) { }
 
     ngOnInit() {
-        this.getUserProfile()
+       // this.getUserProfile()
     }
-
+    /*
     getUserProfile() {
         this.userService.getProfile().subscribe(
             response => {
@@ -24,4 +34,5 @@ export class UserComponent implements OnInit {
             }
         )
     };
+    */
 }
