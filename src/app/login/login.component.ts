68232e08212705fdd92d3bc21f8add5bedb7a959
Florent Nguyen <nguyen.florent@gmail.com>
2025-04-28 13:50:41 +0200

    y

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
 import { AuthService } from '../auth/auth.service';
-import { AppComponent } from '../app.component'
+import { ShellComponent } from '../shell/shell.component'
 
 @Component({
     selector: 'login',
@@ -16,7 +16,7 @@ export class LoginComponent {
         password: new FormControl('', [Validators.required])
     });
     _displayLoginErrorMessage = false;
-    constructor(private app: AppComponent, private router: Router, private authService: AuthService) { }
+    constructor(private app: ShellComponent, private router: Router, private authService: AuthService) { }
 
     // log in button
     onSubmit() {
