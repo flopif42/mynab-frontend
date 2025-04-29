b28e681df2bd7e9ead9ed9b2907b83dca7ef92eb
Florent Nguyen <nguyen.florent@gmail.com>
2025-04-29 18:02:11 +0200

    x

--- src/app/account/dialogs/add-account-dialog.component.ts
+++ src/app/account/dialogs/add-account-dialog.component.ts
@@ -1,25 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ReactiveFormsModule, FormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { MatDialogActions, MatDialogContent, MatDialogRef, MatDialogTitle, } from '@angular/material/dialog';
+import { MatDialogActions, MatDialogContent, MatDialogRef, MatDialogTitle } from '@angular/material/dialog';
 import { materialImports } from '../../utils/material'
 
 @Component({
     selector: 'add-account-dialog',
     templateUrl: 'add-account-dialog.component.html',
-    imports: [
-        FormsModule,
-        ReactiveFormsModule,
-
-        materialImports/*,
-        
-        MatFormFieldModule,
-        MatInputModule,
-        MatButtonModule,
-        MatDialogTitle,
-        MatDialogContent,
-        MatDialogActions
-        */
-    ]
+    imports: [FormsModule, ReactiveFormsModule, materialImports]
 })
 export class AddAccountDialogComponent implements OnInit {
     _addAccountForm: FormGroup;
@@ -28,7 +15,9 @@ export class AddAccountDialogComponent implements OnInit {
 
     ngOnInit() {
         this._addAccountForm = this.fb.group({
-            account_name: ['', [Validators.required]]
+            account_name: ['', Validators.required],
+            account_type: ['', Validators.required],
+            account_balance: ['', [Validators.required, Validators.pattern(/^-?\d+(\.\d{1,2})?$/)]]
         });
     }
 
