0d7af53cef98447124ccae5fa4fb41414a789aac
Florent Nguyen <nguyen.florent@gmail.com>
2025-04-29 12:02:01 +0200

    y

--- src/app/utils/material.ts
+++ src/app/utils/material.ts
@@ -3,11 +3,13 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 export const materialImports = [
     MatSidenavModule,
     MatListModule,
     MatIconModule,
     MatButtonModule,
-    MatDialogModule
+    MatDialogModule,
+    MatFormFieldModule
 ];
