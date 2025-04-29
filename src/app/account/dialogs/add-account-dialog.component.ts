dc88c77e2914b1707da9db808ed5228850262ea4
Florent Nguyen <nguyen.florent@gmail.com>
2025-04-29 11:08:34 +0200

    x

--- src/app/account/dialogs/add-account-dialog.component.ts
+++ src/app/account/dialogs/add-account-dialog.component.ts
@@ -1,5 +1,5 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
-import { materialImports } from '../utils/material';
+import { materialImports } from '../../utils/material';
 
 @Component({
     selector: 'add-account-dialog',
