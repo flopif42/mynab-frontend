95697ed5e96dc9106bd92fd6c9bb2e8965e3f17c
Florent Nguyen <nguyen.florent@gmail.com>
2025-04-29 14:36:13 +0200

    z

--- src/app/account/dialogs/add-account-dialog.component.ts
+++ src/app/account/dialogs/add-account-dialog.component.ts
@@ -1,6 +1,7 @@
-import { Validators, FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, Validators, FormGroup, FormControl } from '@angular/forms';
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { materialImports } from '../../utils/material';
+import { MatDialogRef } from '@angular/material/dialog';
 
 export const addAccountDialogConfig = {
     width: '360px',
@@ -14,9 +15,24 @@ export const addAccountDialogConfig = {
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AddAccountDialogComponent {
-    _addAccountForm = new FormGroup({
-        account_type: new FormControl(1, [Validators.required]),
-        account_name: new FormControl('', [Validators.required]),
-        account_balance: new FormControl('', ['', [Validators.required, Validators.pattern(/^-?\d+(\.\d{1,2})?$/)]])
-    });
+    _addAccountForm = new FormGroup();
+
+    constructor(private fb: FormBuilder, private dialogRef: MatDialogRef<AddAccountDialogComponent>) {
+        this._addAccountForm = this.fb.group({
+            account_type: new FormControl(1, [Validators.required]),
+            account_name: new FormControl('', [Validators.required]),
+            account_balance: new FormControl('', ['', [Validators.required, Validators.pattern(/^-?\d+(\.\d{1,2})?$/)]])
+        });
+    }
+
+    onSubmit() {
+        if (this._addAccountForm.valid) {
+            this.dialogRef.close(this._addAccountForm.value);
+        }
+    }
+
+    onCancel() {
+        this.dialogRef.close();
+    }
 }
+
